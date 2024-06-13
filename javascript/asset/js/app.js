0a6b2cd6ec445bfc0c81836c49faf91d6a80c7eb
Sameer Kharel <110817285+Kharelsameer@users.noreply.github.com>
2024-06-13 20:46:20 +1000

    Add counter

--- javascript/asset/js/app.js
+++ javascript/asset/js/app.js
@@ -1,21 +1,41 @@
-document.querySelector('.counter-wrapper').classList.add('round-box');
+document.querySelector(".counter-wrapper").classList.add("round-box");
 
 let counter = 0;
 
+document.querySelector("#plus").addEventListener("click", addCounter);
+document.querySelector("#minus").addEventListener("click", subCounter);
+document.querySelector("#reset").addEventListener("click", resetCounter);
 
-
-document.querySelector('#plus').addEventListener('click', addCounter);
+function getStep() {
+  const step = document.getElementById("stepBy").value;
+  return step;
+}
 
 function addCounter() {
-counter = counter + 1;
-document.querySelector('.result').innerHTML = counter;
+  newValue = counter + parseInt(getStep());
+  if(newValue > getMax()){
+    const errorMessage = `Counter value cannot exceed the max value ${getMax()}`;
+    document.querySelector(".error-message").innerHTML = errorMessage;
+  }
+  else{
+    counter = newValue;
+    document.querySelector(".result").innerHTML = counter;
+  }
+  
+}
+
+function subCounter() {
+  counter = counter - parseInt(getStep());
+  document.querySelector(".result").innerHTML = counter;
+  document.querySelector(".error-message").innerHTML = '';
 
 }
 
-document.querySelector('#minus').addEventListener('click', subCounter);
+function resetCounter() {
+  counter = 0;
+  document.querySelector(".result").innerHTML = counter;
+}
 
-function subCounter() {
-counter = counter - 1;
-document.querySelector('.result').innerHTML = counter;
-
+function getMax(){
+return document.getElementById("max").value;
 }
