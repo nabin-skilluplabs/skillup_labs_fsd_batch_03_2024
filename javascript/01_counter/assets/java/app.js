20a433d51404b8ac5267ad2dcc55eb0a79897d3d
Panesur <surajneupane02@gmail.com>
2024-06-13 19:40:46 +1000

    java script

---
+++ javascript/01_counter/assets/java/app.js
@@ -0,0 +1,19 @@
+document.querySelector('.counter-wrapper').classList.add ('round-box') ;
+
+let counter = 0;
+
+
+document.getElementById("plus").addEventListener('click', addCounter) ;
+
+function addCounter() {
+    counter = counter + 1;
+    document.querySelector('.result').innerHTML = counter;
+    
+}
+
+document.getElementById("minus").addEventListener('click', subCounter);
+
+function subCounter () {
+    counter = counter - 1;
+    document.querySelector('.result').innerHTML = counter;
+}
