7758d5c7adf4eb7ab621246d13a05e4300b191f9
Panesur <surajneupane02@gmail.com>
2024-06-13 20:49:37 +1000

    js update

--- javascript/01_counter/assets/java/app.js
+++ javascript/01_counter/assets/java/app.js
@@ -1,19 +1,43 @@
 document.querySelector('.counter-wrapper').classList.add ('round-box') ;
 
 let counter = 0;
-
-
 document.getElementById("plus").addEventListener('click', addCounter) ;
+document.getElementById("minus").addEventListener('click', subCounter);
+document.getElementById("reset").addEventListener ('click', resetCounter);
+
+
 
 function addCounter() {
-    counter = counter + 1;
-    document.querySelector('.result').innerHTML = counter;
-    
+    const newValue = counter + parseInt(getStep());
+    if(counter >= getMax()) {
+        const errorMessage = 'Counter value cannot be more that max value 100';
+        document.querySelector('.error-message').innerHTML = errorMessage;
+    }
+    else {
+        counter = newValue;
+        document.querySelector('.result').innerHTML = counter;
+    }
 }
 
-document.getElementById("minus").addEventListener('click', subCounter);
 
 function subCounter () {
-    counter = counter - 1;
+    counter = counter - parseInt(getStep());
     document.querySelector('.result').innerHTML = counter;
+    document.querySelector('.error-message').innerHTML = '';
+}
+
+
+function resetCounter () {
+    counter = 0;
+    document.querySelector('.result').innerHTML = counter;
+}
+
+
+function getStep() {
+    const step = document.getElementById('stepBy').value;
+    return step
+}
+
+function getMax() {
+    return document.getElementById('max').value;
 }
