8251e54acad7cf89c0a77df6bb42e2daf0f39690
Yam Khadka <165254553+Yam556@users.noreply.github.com>
2024-06-14 20:54:18 +1000

    update rules

--- javascript/js/day2.js
+++ javascript/js/day2.js
@@ -1,2 +1,42 @@
-console.log(a)
-let a = 1
+//create three variables to store name, email, mobile and print them in the console
+let name = 'Yam';
+let email = 'yamk6285@gmail.com';
+let mobile = '0426733324';
+
+console.log(name);
+console.log(email);
+console.log(mobile);
+
+//reassign different values to email and mobile
+
+email = "khadka123@gmail.com";
+mobile = "0424833367";
+console.log(email);
+console.log(mobile);
+
+//create two constants to store college name and address
+
+const collegename = 'SABT'
+const address = 'museum'
+console.log(collegename);
+console.log(address);
+
+//try to reassign different values to address constant
+//address = 'george st'
+
+function sayHello(){
+    let greeting = `hello ${name}`;
+    console.log(greeting)
+}
+sayHello()
+
+//undefined is shown up if the variable is defined by var but with no value as below example 
+
+console.log(program)
+ var program = "fsd";
+//
+console.log(document);
+console.log(window.document);
+//both of them are same define that have same result
+
+console.log(globalThis.document)
