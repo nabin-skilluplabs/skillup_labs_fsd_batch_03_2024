3d8ae576c7e711928b0d2b5874119e12cdd1e7e8
Nabin Singh <nabin@skilluplabs.com.au>
2024-06-15 16:19:51 +1000

    Javascript

---
+++ javascript/day_02/assets/js/app.js
@@ -0,0 +1,30 @@
+// Create three variables to store name, email and mobile and print them in the console
+let name = 'Alex';
+let email = 'alex@gmail.com';
+let mobile = '04203004546';
+console.log(name);
+console.log(email);
+console.log(mobile);
+// Reassign different values to email and mobile
+email = "nexalex@gmail.com";
+mobile = '0420400123';
+console.log(email);
+console.log(mobile);
+// Create two constants to store college name and address
+const collegeName = 'Kent';
+const address = 'York St';
+console.log(collegeName);
+console.log(address);
+// Try to reassign different values to address constant
+// address = 'George St';
+
+sayHello()
+function sayHello() {
+    let greeting = `Hello ${name}`;
+    console.log(greeting);
+}
+
+let program = 'FSD';
+console.log(document)
+console.log(window)
+console.log(globalThis)
