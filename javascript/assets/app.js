a56477b515309805cb412a8299213436e9466672
sandhya063 <165256047+sandhya063@users.noreply.github.com>
2024-06-16 15:24:14 +1000

    made changes in javascript

---
+++ javascript/assets/app.js
@@ -0,0 +1,45 @@
+// Create three variables to store name, email and mobile and print them in console module level scope
+
+let firstName = 'Sandhya'
+let email = 'sandhyaaryal777@gmail.com'
+let mobile = '0410379153'
+ console.log(firstName)
+ console.log(email)
+ console.log(mobile)
+
+// Reassign different values to email and mobile
+firstName = 'Ocean'
+email = 'ocean@gmail.com'
+mobile = '03465858'
+console.log(firstName)
+console.log(email)
+console.log(mobile)
+
+
+
+// Create two constants to store college name and address
+const collegeName = 'KENT'
+const address = 'Wynyard'
+console.log(collegeName)
+console.log(address)
+// Try to reassign different values to adress constant
+//collegeName = 'KOI'
+//address = 'Cremorne'
+//console.log(collegeName)
+//console.log(address)
+
+function sayHello() {
+    let greeting = `Hello ${firstName}`
+    console.log(greeting)
+}
+
+sayHello()
+
+
+var program = 11
+console.log(program)
+
+
+
+
+
