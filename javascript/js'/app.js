43eb0aff3b51bc9128d806ac335deb748261a369
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-06-21 23:05:15 +1000

    added js

---
+++ javascript/js'/app.js
@@ -0,0 +1,86 @@
+console.log('Calculator');
+
+// DOM - Document Object Model
+
+const buttons =  document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', handleClick);
+});
+let calculateExp = '';
+function handleClick(event) {
+    const value = event.target.innerText;
+    if(value === "=") {
+        try {
+            calculateExp = eval(calculateExp);
+        }
+        catch {
+            calculateExp = '';
+            // alert('Invalid calculation, please refresh and try again!');
+        }
+    }
+    else if (value === 'CE') {
+        calculateExp = '';
+    }
+    else if(value === 'DEL') {
+        calculateExp = calculateExp.slice(0, calculateExp.length - 1);
+    }
+    else if((value === '+') || (value === '-') || (value === '*') || (value === '/')) {
+        const lastValue = calculateExp[calculateExp.length - 1];
+        if(parseInt(lastValue) == lastValue) {
+            calculateExp = calculateExp + value;
+        }
+        else {
+            calculateExp = calculateExp.slice(0, calculateExp.length - 1) + value;
+        }
+    }
+    else {
+        calculateExp = calculateExp + value;
+    }
+    document.querySelector('.display').innerText = calculateExp;
+}
+
+let calculateExp = '';
+function handleClick(event) {
+const value = event.target.innerText;
+switch(value) {
+    case "=":
+        {
+        try {
+            calculateExp = eval(calculateExp);
+           }
+           catch {
+            calculateExp = 'enter wrong';
+            
+           }
+        }
+    break;
+    case 'CE':
+             calculateExp = '';
+    break;
+    case 'DEL':
+                calculateExp = calculateExp.slice(0, calculateExp.length - 1);
+    break;
+    case '+':
+    case '-':
+    case '*':
+    case '%':
+                const lastValue = calculateExp[calculateExp.length - 1];
+                if(parseInt(lastValue) == lastValue) {
+                calculateExp = calculateExp + value;
+
+            }
+                else {
+                calculateExp = calculateExp.slice(0, calculateExp.length - 1) + value;
+            }
+    break;
+    default: 
+    calculteExp = calculateExp + value;
+        }
+    console.log(calculateExp);
+    document.querySelector('.display').innerHTML = calculateExp;
+}
+
+
+
+
+
