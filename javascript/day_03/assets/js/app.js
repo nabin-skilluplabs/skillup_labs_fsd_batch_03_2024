fba666c30f6b3fc401e306e8cba30f0afe27499a
Nabin Singh <nabin@skilluplabs.com.au>
2024-06-21 18:33:01 +1000

    Add calculator app

---
+++ javascript/day_03/assets/js/app.js
@@ -0,0 +1,40 @@
+console.log('Calculator');
+
+// DOM - Document Object Model
+
+const buttons =  document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', handleClick);
+});
+let calculateExp = '';
+function handleClick(event) {
+    const value = event.target.innerText;
+    if(value === "=") {
+        try {
+            calculateExp = eval(calculateExp);
+        }
+        catch {
+            calculateExp = '';
+            // alert('Invalid calculation, please refresh and try again!');
+        }
+    }
+    else if (value === 'CE') {
+        calculateExp = '';
+    }
+    else if(value === 'DEL') {
+        calculateExp = calculateExp.slice(0, calculateExp.length - 1);
+    }
+    else if((value === '+') || (value === '-') || (value === '*') || (value === '/')) {
+        const lastValue = calculateExp[calculateExp.length - 1];
+        if(parseInt(lastValue) == lastValue) {
+            calculateExp = calculateExp + value;
+        }
+        else {
+            calculateExp = calculateExp.slice(0, calculateExp.length - 1) + value;
+        }
+    }
+    else {
+        calculateExp = calculateExp + value;
+    }
+    document.querySelector('.display').innerText = calculateExp;
+}
