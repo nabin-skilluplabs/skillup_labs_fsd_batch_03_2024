09568e87c0a16c4d8d74cc98544b06cb7f93e697
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-06-28 21:10:33 +1000

    added js

--- javascript/day-5/assest/js/app.js
+++ javascript/day-5/assest/js/app.js
@@ -1,4 +1,25 @@
-let pincode = '';
-while (pincode !== '2468') {
-    const pincode = prompt('enter pincode: ');
+function addEventListener() {
+     document.querySelector('#addBtn').addEventListener('click', handleAddNew);
 }
+const toDoItems = [];
+function handleAddNew(event) {
+    event.preventDefault();
+
+    const todoItem = document.querySelector('input').value;
+    toDoItems.push(todoItem);
+    populateToDoItems(toDoItems);
+   console.log({todoItem, toDoItems});
+
+
+
+
+}
+function populateToDoItems(toDoItems) {
+    let list = '';
+    for(value of toDoItems) {
+        list += `<div class="courses"><span>${value}</span><input type="checkbox"></input></div>`;
+    }
+    document.querySelector(".course-content").innerHTML = list;
+    document.querySelector("#total").innerHTML = toDoItems.length;
+}
+addEventListener();
