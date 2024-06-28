465fd1bf67076c9438308c3376c6c2e084883e86
Yam Khadka <165254553+Yam556@users.noreply.github.com>
2024-06-28 17:27:33 +1000

    new js

--- javascript/day_5/assets/js/app.js
+++ javascript/day_5/assets/js/app.js
@@ -1,5 +1,25 @@
 let pincode = '';
 while(pincode !== '1234') {
     pincode = prompt('Enter Pincode')
-
 }
+
+let grid = 15;
+function createPattern(grid){
+    let pattern = '';
+    for(let row = 0; row < grid; row++){
+        for(let col = 0; col < grid; col++){
+            if((row + col) % 2 === 0){
+                pattern += ''; 
+            }
+            else{
+                pattern += '#';
+            }
+        }
+        if (row < grid - 1){
+            pattern +='\n';
+        }
+    }
+    return pattern;
+}
+
+console.log(createPattern(grid));
