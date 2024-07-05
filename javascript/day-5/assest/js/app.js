a845070f970be9975c67885b0f071129e08b630f
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2024-07-05 19:44:30 +1000

    add js

--- javascript/day-5/assest/js/app.js
+++ javascript/day-5/assest/js/app.js
@@ -6,9 +6,20 @@ function handleAddNew(event) {
     event.preventDefault();
 
     const todoItem = document.querySelector('input').value;
-    toDoItems.push(todoItem);
+    const inputItem = document.querySelector('input').value= '';
+    
+
+
+    if(todoItem !== '') {
+    toDoItems.unshift(todoItem);
     populateToDoItems(toDoItems);
+    inputItem.value = '';
    console.log({todoItem, toDoItems});
+   const bodyElement = document.querySelector('body');
+   const bodyHeight = 
+    
+    }
+  
 
 
 
@@ -20,6 +31,18 @@ function populateToDoItems(toDoItems) {
         list += `<div class="courses"><span>${value}</span><input type="checkbox"></input></div>`;
     }
     document.querySelector(".course-content").innerHTML = list;
+    const allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    allCheckboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener('click', (event) => removeToDoItem(event, index));
+    });
     document.querySelector("#total").innerHTML = toDoItems.length;
+}
+function removeToDoItem(event, index) {
+z
+    if(event.target.checked){
+        toDoItems.splice(index, 1);
+        populateToDoItems(toDoItems);
+
+    }
 }
 addEventListener();
