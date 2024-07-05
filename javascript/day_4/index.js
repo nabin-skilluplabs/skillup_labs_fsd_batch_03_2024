f3860405e65612ee2021217e63990a6bcdd5c7b3
Yam Khadka <165254553+Yam556@users.noreply.github.com>
2024-07-05 20:49:38 +1000

    update

--- javascript/day_4/index.js
+++ javascript/day_4/index.js
@@ -2,18 +2,24 @@
 */
 function addEventListener(){
     document.querySelector("#Addbtn").addEventListener("click", handleAddNew);
+    document.querySelector("#view_all").addEventListener("click", toggleView);
 }
 const todoItems = [];
 /* declaring above variables outside of function to make it easy accessible even inside function*/
 function handleAddNew(event){
     event.preventDefault();
-
-    const todoItem = document.querySelector('input').value;
+    const inputItems = document.querySelector('input');
+    const todoItem = inputItems.value;
     /* above line is to add items to the list that we type in the input field */
-    todoItems.push(todoItem);
-    populateToDoItems(todoItems);
+    
+    if(todoItem !== ""){
+    todoItems.unshift(todoItem);
+    populateToDoItems(todoItems.slice(0, 5));
+    inputItems.value = '';
     console.log({todoItem, todoItems});
     /* This is to print the result*/
+    }
+    document.querySelector('#total').innerHTML = todoItems.length;
 }
 
 function populateToDoItems(todoItems){
@@ -23,6 +29,39 @@ function populateToDoItems(todoItems){
 
     }
     document.querySelector('.form-text').innerHTML = list;
+    const allCheckboxes = document.querySelectorAll("input[type='checkbox']");
+    allCheckboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener('click', (event => removeToDoItem(event, index)));
+    })
+    
+}
+
+function showTotalCount(){
     document.querySelector('#total').innerHTML = todoItems.length;
+}
+function removeToDoItem(event, index){
+    if(event.target.checked){
+        todoItems.splice(index, 1);
+        const textContent = document.querySelector('#view_all').textContent; 
+        if(textContent === 'View All'){
+            populateToDoItems(todoItems.slice(0, 5));
+        }
+        else{
+            populateToDoItems(todoItems);
+        }
+        showTotalCount();
+    }
+    
+}
+function toggleView(){
+    const textContent = document.querySelector('#view_all').textContent; 
+    if(textContent === 'View All'){
+        populateToDoItems(todoItems);
+        document.querySelector('#view_all').textContent = 'View Less';
+    }
+    else {
+        populateToDoItems(todoItems.slice(0, 5));
+        document.querySelector('#view_all').textContent = 'View All';
+    }
 }
 addEventListener()
