366b5bb7fc3395d5a72ac6cc653e4e999781f13f
sandhya063 <165256047+sandhya063@users.noreply.github.com>
2024-07-08 15:49:09 +1000

    made chnages

--- javascript/day_06/assets/js/app.js
+++ javascript/day_06/assets/js/app.js
@@ -1,28 +1,79 @@
 function addEventListener() {
     document.querySelector("#addBtn").addEventListener("click", handleAddNew);
+    document.querySelector("#view_all").addEventListener("click", toggleView)
 }
 const toDoItems = [];
-
 function handleAddNew(event) {
     event.preventDefault();
+    const inputItem = document.querySelector('input');
+    const todoItem =  inputItem.value;
+    if(todoItem !== "") {
+        toDoItems.unshift(todoItem);
+        populateToDoItems(toDoItems.slice(0,5));
+        inputItem.value = "";
 
-    const todoItem =  document.querySelector('input').value;
-    toDoItems.push( todoItem);
-    populateToDoItems(toDoItems, toDoItems);
-    console.log({todoItem});
+
+        console.log({todoItem, toDoItems});
+
+        
+    }
 }
+//conditional- use if or ifelse 
+//shift-removes last unshift- add last push-add in first and pop-remove in first
 
 function populateToDoItems(toDoItems) {
-    let list = '';
+    let list  = '';
     for(value of toDoItems) {
-        list += `<div><span>${value}</span> <input type="ckeckbox" ></div>`;
+        list += `<div><span>${value}</span> <input type="checkbox" ></div>`;
     }
     document.querySelector(".list").innerHTML = list;
+    const allCheckboxes = document.querySelectorAll("input[type='checkbox']");
+    allCheckboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener('click' , (event) =>  removeToDoItem(event, index));
+    });
     document.querySelector("#total").innerHTML = toDoItems.length;
 }
 
+function showTotalCount() {
+    document.querySelector("#total").innerHTML = toDoItems.length;
+}
+function removeToDoItem(event , index) {
+    console.log(event.target);
+    if(event.target.checked) {
+        toDoItems.splice(index, 1);
+        const textContent = document.querySelector("#view_all").textContent;
+        if (textContent === 'View All') {
+        populateToDoItems(toDoItems.slice(0,5));
+        }
+        else {
+            populateToDoItems(toDoItems);
+        }
+        showTotalCount
 
-addEventListener()
+        console.log({index})
+
+    }
+}
+function toggleView() {
+
+    const textContent = document.querySelector("#view_all").textContent;
+    if(textContent === "View All")  {
+        populateToDoItems(toDoItems);
+        if(toDoItems.length > 5) {
+            document.querySelector('#view_all').textContent = "View less";
+        }
+    } 
+    else{
+    
+    populateToDoItems(toDoItems.slice(0,5));
+
+    document.querySelector("#view_all").textContent = "View All";
+    
+       
+    }
+}
+
+addEventListener();
 
 
 
